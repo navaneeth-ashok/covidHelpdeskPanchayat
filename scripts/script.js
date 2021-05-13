354046ecfb265eb8f1b2ec1d0e550f09a4bb6a01
navaneeth_ashok_1 <navaneeth619@gmail.com>
2021-05-14 01:54:42 +0530

    Adding all files for upload

---
+++ scripts/script.js
@@ -0,0 +1,51 @@
+var $cell = $(".card");
+
+var gridStatusStock;
+var deviceWidthThreshold = 480;
+
+//open and close card when clicked on card
+$cell.find(".js-expander").click(function () {
+  var $thisCell = $(this).closest(".card");
+
+  if ($thisCell.hasClass("is-collapsed")) {
+    $cell.not($thisCell).removeClass("is-expanded").addClass("is-collapsed");
+    //   .addClass("is-inactive");
+    $thisCell.removeClass("is-collapsed").addClass("is-expanded");
+    if ($(window).width() <= deviceWidthThreshold) {
+      gridStatusStock =
+        document.getElementsByClassName("grid-container")[0].style
+          .gridTemplateColumns;
+
+      document.getElementsByClassName(
+        "grid-container"
+      )[0].style.gridTemplateColumns = "auto";
+    }
+
+    if ($cell.not($thisCell).hasClass("is-inactive")) {
+      //do nothing
+    } else {
+      //   $cell.not($thisCell).addClass("is-inactive");
+    }
+  } else {
+    $thisCell.removeClass("is-expanded").addClass("is-collapsed");
+    $cell.not($thisCell).removeClass("is-inactive");
+    if ($(window).width() <= deviceWidthThreshold) {
+      document.getElementsByClassName(
+        "grid-container"
+      )[0].style.gridTemplateColumns = gridStatusStock;
+    }
+  }
+});
+
+//close card when click on cross
+$cell.find(".js-collapser").click(function () {
+  var $thisCell = $(this).closest(".card");
+
+  $thisCell.removeClass("is-expanded").addClass("is-collapsed");
+  $cell.not($thisCell).removeClass("is-inactive");
+  if ($(window).width() <= "480") {
+    document.getElementsByClassName(
+      "grid-container"
+    )[0].style.gridTemplateColumns = gridStatusStock;
+  }
+});
