2e9b8e9c38f9f5113a8db7e69a20c04fcca3256c
navaneeth_ashok_1 <navaneeth619@gmail.com>
2021-05-22 13:13:05 +0530

    Fixed PWA path

--- scripts/script.js
+++ scripts/script.js
@@ -156,7 +156,7 @@ function toggleTheme() {
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("/serviceWorker.js")
+    .register("serviceWorker.js")
     .then((reg) => console.log("service worker registered"))
     .catch((err) => console.log("service worker not registered", err));
 }
